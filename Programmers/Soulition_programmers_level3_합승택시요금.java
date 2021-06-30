92b1633d57a91ad7c54ceecfca4f773f8784d2e7
taeho0304 <tdj03063@gmail.com>
2021-06-30 17:15:19 +0900

    210630

--- Programmers/Soulition_programmers_level3_합승택시요금.java
+++ Programmers/Soulition_programmers_level3_합승택시요금.java
@@ -1,113 +1,82 @@
 package programmers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class Soulition_programmers_level3_합승택시요금 {
-  static class Node implements Comparable<Node> {
-    //
-    int idx, dist;
+	private static ArrayList<Node>[] list;
 
-    public Node(int idx, int dist) {
-      this.idx = idx;
-      this.dist = dist;
-    }
+	public static void main(String[] args) {
+		int n = 7, s = 4, a = 6, b = 2;
+		int[][] fares = new int[][] { { 4, 1, 10 }, { 3, 5, 24 }, { 5, 6, 2 }, { 3, 1, 41 }, { 5, 1, 24 }, { 4, 6, 50 },
+				{ 2, 4, 66 }, { 2, 3, 22 }, { 1, 6, 25 } };
 
-    @Override
-    public int compareTo(Node o) {
-      return Integer.compare(this.dist, o.dist);
-    }
-  }
+		////////////////////////////////////////////////////////
 
-  static int N, INF;
-  static int[][] graph;
+		list = new ArrayList[n + 1];
+		for (int i = 1; i <= n; i++)
+			list[i] = new ArrayList<>();
 
-  public static void main(String[] args) {
-    int[][] fares = new int[][] { { 4, 1, 10 }, { 3, 5, 24 }, { 5, 6, 2 }, { 3, 1, 41 }, { 5, 1, 24 }, { 4, 6, 50 },
-        { 2, 4, 66 }, { 2, 3, 22 }, { 1, 6, 25 } };
-    System.out.println(solution(6, 4, 6, 2, fares));
+		for (int i = 0; i < fares.length; i++) {
+			int start = fares[i][0]; // 출발지
+			int end = fares[i][1]; // 목적지
+			int cost = fares[i][2]; // 비용
 
-  }
+			// 양방향 이동 포함해서 그래프 생성
+			list[start].add(new Node(end, cost));
+			list[end].add(new Node(start, cost));
+		}
 
-  static int solution(int n, int s, int a, int b, int[][] fares) {
-    N = n;
-    INF = 987654321;
-    graph = new int[N + 1][N + 1];
+		int[] dS = new int[n + 1]; // 출발지에서 합승지점까지
+		int[] dA = new int[n + 1]; // 합승지점에서 A까지
+		int[] dB = new int[n + 1]; // 합승지점에서 B까지
 
-    // 연결되어있지 않은 노드를 표시하기 위해 최대값으로 초기화
-    for (int i = 0; i <= N; i++) {
-      Arrays.fill(graph[i], INF);
-    }
+		dS = dijk(s, dS); // 다익스트라 알고리즘을 각 정점까지 최단거리 계산
+		dA = dijk(a, dA);
+		dB = dijk(b, dB);
 
-    for (int[] far : fares) {
-      int n1 = far[0];
-      int n2 = far[1];
-      int dist = far[2];
+		int answer = Integer.MAX_VALUE;
+		for (int i = 1; i <= n; i++) 
+			answer = Math.min(answer, dS[i] + dA[i] + dB[i]); // 정답 추출
 
-      // 양방향 그래프 적용
-      graph[n1][n2] = dist;
-      graph[n2][n1] = dist;
-    }
+		System.out.println(answer);
+	}
 
-    // 출발지와 도착지를 시작점으로 잡고 모든 정점에 대한 최소 이동 경로 구하기
-    // 양방향이기 때문에 sDist[a] = s <->
-    int[] sDist = dijkstra(s);
-    int[] aDist = dijkstra(a);
-    int[] bDist = dijkstra(b);
-    
-    // 1. 합승하지 않고 각자 따로 갔을 때 : S -> A, S -> B
-    int noShare = sDist[a] + sDist[b];
+	static int[] dijk(int start, int[] dist) {
+		Arrays.fill(dist, Integer.MAX_VALUE); // 거리 배열을 최댓값으로 초기화
+		dist[start] = 0; // 시작 비용 0
+		PriorityQueue<Node> q = new PriorityQueue<>(); // 큐에 넣을 횟수를 최소화 시키기 위해 pq 사용
+		q.offer(new Node(start, 0));
 
-    // 2. 합승해서 가기 S -> i + i -> A + i -> B
-    int share = Integer.MAX_VALUE;
-    for (int i = 1; i <= N; i++) {
-      // 출발지에서 경유지로 가는 길이나 경유지에서 도착지로 가는 길이 하나라도 없으면 이동할 수 없음
-      if (sDist[i] == INF || aDist[i] == INF || bDist[i] == INF)
-        continue;
+		while (!q.isEmpty()) {
+			Node node = q.poll();
+			for (int i = 0; i < list[node.to].size(); i++) { // 현재 노드에서 갈 수 있는 모든 노드 만큼
+				int to = list[node.to].get(i).to; // 다음 목적지
+				int cost = list[node.to].get(i).cost; // 다음 목적지까지 비용
 
-      // 출발지에서 경유지 i까지 같이 가고, i에서부터 각자 도착지 A, B로 가는 경우의 수 중 최소값
-      share = Math.min(share, sDist[i] + aDist[i] + bDist[i]);
-    }
+				if (dist[to] > node.cost + cost) { //더 작으면 갱신하고 큐에 넣음
+					dist[to] = node.cost + cost;
+					q.offer(new Node(to, dist[to]));
+				}
+			}
+		}
+		return dist;
+	}
+}
 
-    return Math.min(noShare, share);
-  }
+class Node implements Comparable<Node> {
+	int to; // 목적지
+	int cost; // 비용
 
-  /**
-   * 매개변수 from : 출발지
-   * 다익스트라 : 출발지 ~ 모든 정점으로의 최소 이동 경로
-   **/
-  public static int[] dijkstra(int from) {
-    PriorityQueue<Node> pq = new PriorityQueue<Node>();
-    // from에서 각 도착지로 가기 위한 최소 경로 값 들어있음
-    int[] dist = new int[N + 1];
-    boolean[] visited = new boolean[N + 1];
+	Node(int to, int cost) {
+		this.to = to;
+		this.cost = cost;
+	}
 
-    Arrays.fill(dist, INF);
-
-    dist[from] = 0;
-    pq.add(new Node(from, dist[from]));
-
-    while (!pq.isEmpty()) {
-      int here = pq.peek().idx;
-      int cost = pq.peek().dist;
-      pq.poll();
-
-      if (cost > dist[here])
-        continue;
-
-      for (int i = 1; i <= N; i++) {
-        if (graph[here][i] != INF && !visited[i]) {
-          if (dist[i] > (dist[here] + graph[here][i])) {
-            dist[i] = dist[here] + graph[here][i];
-            pq.add(new Node(i, dist[i]));
-          }
-        }
-      }
-
-      visited[here] = true;
-    }
-
-    return dist;
-  }
-
+	@Override
+	public int compareTo(Node o) {
+		// TODO Auto-generated method stub
+		return cost - o.cost;
+	}
 }
