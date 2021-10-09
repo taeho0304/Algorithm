9b97805198add157d2316fc65a7fe438ba0a157f
taeho0304 <tdj03063@gmail.com>
2021-10-10 02:15:26 +0900

    20121010

--- Programmers/Soulition_programmers_level2_기능개발.java
+++ Programmers/Soulition_programmers_level2_기능개발.java
@@ -2,35 +2,37 @@ package programmers;
 
 import java.util.*;
 
-public class Soulition_programmers_level2_기능개발 {
-
-	public static void main(String[] args) {
-
-		int bridge_length = 2; // 다리길이
-		int weight = 10; // 다리가 견딜 수 있는 무게
-		int[] truck_weights = { 7, 4, 5, 6 };// 트럭별 무게
-
-		/////////////////////////////////////////////////////
-
-		int answer = 0, cur = 0;
-		Queue<Integer> onBridge = new LinkedList<Integer>();
-		int endTime[] = new int[truck_weights.length];
-
-		while (true) {
-			if (!onBridge.isEmpty() && endTime[onBridge.peek()] == answer) {// 다리를 지난 트럭 제거
-				weight += truck_weights[onBridge.poll()];
-			}
-
-			if (cur < truck_weights.length && truck_weights[cur] <= weight) {// 다리에 트럭 추가
-				onBridge.add(cur);
-				endTime[cur] = answer + bridge_length;
-				weight -= truck_weights[cur];
-				cur++;
-			}
-			answer++;
-			if (onBridge.isEmpty())
-				break;
-		}
-		System.out.println(answer);
-	}
+class Soulition_programmers_level2_기능개발 {
+    public int[] solution(int[] progresses, int[] speeds) {
+        
+        List<Integer> ans = new ArrayList<>();
+        Queue<Integer> time = new LinkedList<>();
+        
+        for(int i=0; i<progresses.length; i++){
+            int t=0;
+            int work = progresses[i];
+            while(work<100){
+                work+=speeds[i];
+                t++;
+            }
+            time.add(t);
+        }
+        
+        while(!time.isEmpty()){
+            int t = time.poll();
+            int cnt = 1;
+            while(!time.isEmpty() && time.peek()<=t){
+                cnt++;
+                time.poll();
+            }
+            ans.add(cnt);
+        }
+        
+        int[] answer = new int[ans.size()];
+        for(int i=0; i<ans.size(); i++)
+            answer[i] = ans.get(i);
+        
+        
+        return answer;
+    }
 }
