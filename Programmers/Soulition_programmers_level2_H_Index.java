6b28bacdba7ac7cc58503d80f383c86bc64c1178
taeho0304 <tdj03063@gmail.com>
2021-12-29 22:40:11 +0900

    20211229

---
+++ Programmers/Soulition_programmers_level2_H_Index.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class H_Index {
+	public static void main(String[] args) {
+
+		int[] citations = {3, 0, 6, 1, 5};
+		
+		/////////////////////////
+		
+		int answer = 0;
+        Arrays.sort(citations);
+ 
+        for (int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
+ 
+            if (citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+ 
+        System.out.println(answer);
+	}
+}
